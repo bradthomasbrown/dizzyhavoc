2a8281d1723d93cfa0a72af5cbad257ae7d1b9e2
Brad Brown <bradbrown@bradbrown.llc>
2024-01-09 22:24:51 -0500

    an erc20 implementation for dizzyhavoc

---
+++ contracts/ERC20/ERC20.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: 0BSD
+pragma solidity 0.8.23;
+
+contract ERC20 {
+
+    function name() external pure returns (string memory) { return "DizzyHavoc"; }
+    function symbol() external pure returns (string memory) { return "DZHV"; }
+    function decimals() external pure returns (uint8) { return 18; }
+    function totalSupply() external pure returns (uint) { return 1e18 * 1e8; }
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+
+    function initErc20() external {
+        require(balanceOf[msg.sender] == 0 && msg.sender == 0xC9c423f875677351ab79C058eD1C38F2b36061a4);
+        uint amount = this.totalSupply();
+        balanceOf[msg.sender] = amount;
+        emit Transfer(address(0), msg.sender, amount);
+    }
+
+    function transfer(address to, uint amount) external {
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+        emit Transfer(msg.sender, to, amount);
+    }
+
+    function transferFrom(address from, address to, uint amount) external {
+        allowance[from][msg.sender] -= amount;
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+        emit Transfer(from, to, amount);
+    }
+
+    function approve(address spender, uint amount) external {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+    }
+
+}
