d8936198edea1620295f7a9ef220d6b9e796e224
Brad Brown <bradbrown@bradbrown.llc>
2024-02-01 17:19:44 -0500

    old resolver contract

--- contracts/SetServers/SetServers.sol
+++ contracts/SetServers/SetServers.sol
@@ -4,19 +4,26 @@ pragma solidity 0.8.23;
 struct Config {
     address server;
     uint32[] selectors;
+    bytes init;
 }
 
 contract SetServers {
+
+    address owner;
+    
     function setServers(Config[] calldata configs) external {
-        require(msg.sender == 0xC9c423f875677351ab79C058eD1C38F2b36061a4);
+        require(msg.sender == owner);
         for (uint i = 0; i < configs.length; i++) {
             Config calldata config = configs[i];
             address server = config.server;
             uint32[] calldata selectors = config.selectors;
+            bytes calldata init = config.init;
             for (uint j = 0; j < selectors.length; j++) {
                 uint32 selector = selectors[j]; // selector is equal to slot
                 assembly { sstore(selector, server) } 
             }
+            assembly { pop(call(gas(), server, 0, init.offset, init.length, 0, 0)) }
         }
     }
+    
 }
