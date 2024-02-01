dad8ef82434a7914dacee760534bcd7ca738f720
Brad Brown <bradbrown@bradbrown.llc>
2024-02-01 17:21:51 -0500

    debug_traceCall ejra impl

--- ejra/traceCall.ts
+++ ejra/traceCall.ts
@@ -1,4 +1,5 @@
 import { z } from 'https://deno.land/x/zod@v3.22.4/mod.ts'
+import ejrc from './ejrc.ts'
 
 type TraceTxConfig = {
     disableStorage?:boolean
@@ -7,11 +8,12 @@ type TraceTxConfig = {
     enableReturnData?:boolean
     tracer?:string
 }
-type Opts = { call:Call, tag?:Tag, config?:TraceTxConfig }
+type Opts = { tx:Partial<Call>, tag?:Tag, config?:TraceTxConfig }
 
-export default function ({ call, tag='latest', config={ enableMemory:true, enableReturnData:true } }:Opts) {
+export default function ({ tx, tag='latest', config={ enableMemory:true, enableReturnData:true } }:Opts) {
     const schema = z.any()
-    const params = [call, tag, config] as const
+    const params = [tx, tag, config] as const
     const method = 'debug_traceCall' as const
-    return { method, params, schema }
+    const ejrrq = { method, params, schema }
+    return { ejrrq, call: (url:string) => ejrc({ url, ejrrq }) }
 }
