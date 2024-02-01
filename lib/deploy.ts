873dc25a21c5cc50852a9a15cfc491f6d45e039e
Brad Brown <bradbrown@bradbrown.llc>
2024-02-01 17:22:32 -0500

    contract deploying shorthand, i think deprecated

--- lib/deploy.ts
+++ lib/deploy.ts
@@ -5,28 +5,58 @@ import jsSha3 from 'npm:js-sha3@0.9.2'
 import { hexToBytes } from 'npm:@noble/hashes@1.3.3/utils'
 const { keccak256 } = jsSha3
 
+// type Opts = {
+//     url:string
+//     signer:Signer,
+//     bytecode:string,
+//     chainId:bigint,
+//     nonce:bigint,
+//     gasPrice:bigint,
+//     gasLimit:bigint
+// }
+
+// export default function ({
+//     signer,
+//     bytecode,
+//     chainId,
+//     nonce,
+//     gasPrice,
+//     gasLimit
+// }:Opts) {
+//     const accessList:[address:string,slots:string[]][] = []
+//     const stx = signer.signTx({ chainId, nonce, gasPrice, gasLimit, data: bytecode, accessList })
+//     const ejrrq = e.sendRawTx({ data: stx })
+//     const txHash = `0x${keccak256(hexToBytes(stx.slice(2)))}`
+//     const address = `0x${keccak256(encode([signer.address, nonce])).slice(-40)}`
+//     return { ejrrq, txHash, address }
+// }
+
 type Opts = {
+    signer:Signer
+    code:string
     url:string
-    signer:Signer,
-    bytecode:string,
-    chainId:bigint,
-    nonce:bigint,
-    gasPrice:bigint,
+    chainId:bigint
+    nonce:bigint
+    gasPrice:bigint
     gasLimit:bigint
+    accessList?:[address:string,slots:string[]][]
 }
 
 export default function ({
     signer,
-    bytecode,
+    code,
     chainId,
     nonce,
     gasPrice,
-    gasLimit
+    gasLimit,
+    accessList=[]
 }:Opts) {
-    const accessList:[address:string,slots:string[]][] = []
-    const stx = signer.signTx({ chainId, nonce, gasPrice, gasLimit, data: bytecode, accessList })
+    const data = code
+    const stx = signer.signTx({ chainId, nonce, gasPrice, gasLimit, data, accessList })
+    const stxBytes = hexToBytes(stx.slice(2))
+    const encoding = encode([signer.address, nonce])
     const ejrrq = e.sendRawTx({ data: stx })
-    const txHash = `0x${keccak256(hexToBytes(stx.slice(2)))}`
-    const address = `0x${keccak256(encode([signer.address, nonce])).slice(-40)}`
-    return { ejrrq, txHash, address }
+    const hash = `0x${keccak256(stxBytes)}`
+    const address = `0x${keccak256(encoding).slice(-40)}`
+    return { ejrrq, hash, address }
 }
