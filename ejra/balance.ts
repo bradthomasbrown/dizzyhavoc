753d98a666cd7258a49a9af67c9529fbde02a0ec
Brad Brown <bradbrown@bradbrown.llc>
2024-02-01 17:20:04 -0500

    eth_getBalance ejra impl

--- ejra/balance.ts
+++ ejra/balance.ts
@@ -1,4 +1,5 @@
 import { z } from 'https://deno.land/x/zod@v3.22.4/mod.ts'
+import ejrc from './ejrc.ts'
 
 type Opts = { address:string, tag?:Tag }
 
@@ -6,5 +7,6 @@ export default function ({ address, tag='latest' }:Opts) {
     const schema = z.string().transform(str => BigInt(str))
     const params = [address, tag] as const
     const method = 'eth_getBalance' as const
-    return { method, params, schema }
+    const ejrrq = { method, params, schema }
+    return { ejrrq, call: (url:string) => ejrc({ url, ejrrq }) }
 }
