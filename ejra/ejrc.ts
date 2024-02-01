e9cf9662eee5c1caba9ecb90b4f7cd668fe31c85
Brad Brown <bradbrown@bradbrown.llc>
2024-02-01 17:20:49 -0500

    ejra call function

--- ejra/ejrc.ts
+++ ejra/ejrc.ts
@@ -1,6 +1,6 @@
 import { z } from 'https://deno.land/x/zod@v3.22.4/mod.ts'
 
-export default function <
+export default async function <
     E extends { method:string, params?:P, schema:S },
     P extends readonly unknown[],
     S extends z.ZodTypeAny
@@ -9,8 +9,12 @@ export default function <
     const body = JSON.stringify(jrrq, (_, v) => typeof v == 'bigint' ? `0x${v.toString(16)}` : v)
     const headers = { 'Content-Type': 'application/json' } as const
     const init = { body, headers, method: 'POST' } as const
-    return fetch(url, init)
-        .then(response => response.json())
-        .then(json => z.object({ result: z.unknown() }).parseAsync(json))
-        .then(({ result }) => schema.parseAsync(result) as z.infer<E['schema']>)
+    const response = await fetch(url, init)
+    const json = await response.json()
+    const { result, error } = await z.object({
+        result: z.unknown(),
+        error: z.object({ message: z.string() }).optional()
+    }).parseAsync(json)
+    if (error) throw new Error(error.message, { cause: JSON.stringify(json) })
+    return schema.parseAsync(result) as z.infer<E['schema']>
 }
