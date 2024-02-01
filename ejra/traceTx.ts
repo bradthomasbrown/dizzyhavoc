52dc5ce202465b5d2e0a3347ba8f1b75fa615fad
Brad Brown <bradbrown@bradbrown.llc>
2024-02-01 17:21:59 -0500

    debug_traceTransaction ejra impl

--- ejra/traceTx.ts
+++ ejra/traceTx.ts
@@ -1,4 +1,5 @@
 import { z } from 'https://deno.land/x/zod@v3.22.4/mod.ts'
+import ejrc from './ejrc.ts'
 
 type TraceTxConfig = {
     disableStorage?:boolean
@@ -7,11 +8,12 @@ type TraceTxConfig = {
     enableReturnData?:boolean
     tracer?:string
 }
-type Opts = { txHash:string, config?:TraceTxConfig }
+type Opts = { hash:string, config?:TraceTxConfig }
 
-export default function ({ txHash, config={ enableMemory:true, enableReturnData:true } }:Opts) {
+export default function ({ hash, config={ enableMemory:true, enableReturnData:true } }:Opts) {
     const schema = z.any()
-    const params = [txHash, config] as const
+    const params = [hash, config] as const
     const method = 'debug_traceTransaction' as const
-    return { method, params, schema }
+    const ejrrq = { method, params, schema }
+    return { ejrrq, call: (url:string) => ejrc({ url, ejrrq }) }
 }
