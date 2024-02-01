a94b7adedbc6a8e05a92e8b855678460f9fd4882
Brad Brown <bradbrown@bradbrown.llc>
2024-02-01 17:21:41 -0500

    eth_sendRawTransaction ejra impl

--- ejra/sendRawTx.ts
+++ ejra/sendRawTx.ts
@@ -1,10 +1,12 @@
 import { z } from 'https://deno.land/x/zod@v3.22.4/mod.ts'
+import ejrc from './ejrc.ts'
 
-type Opts = { data:string }
+type Opts = { signedTx:string }
 
-export default function ({ data }:Opts) {
+export default function ({ signedTx }:Opts) {
     const schema = z.string()
     const method = 'eth_sendRawTransaction'
-    const params = [data] as const
-    return { method, params, schema }
+    const params = [signedTx] as const
+    const ejrrq = { method, params, schema }
+    return { ejrrq, call: (url:string) => ejrc({ url, ejrrq }) }
 }
