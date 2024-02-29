9faa401a11efa0ad20195075f105ec3a5c8ab56e
Brad Brown <bradbrown@bradbrown.llc>
2024-02-29 17:41:24 -0500

    this should be deleted

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -1,16 +1,16 @@
-import type { Signal } from "@preact/signals";
-import { Button } from "../components/Button.tsx";
+// import type { Signal } from "@preact/signals";
+// import { Button } from "../components/Button.tsx";
 
-interface CounterProps {
-  count: Signal<number>;
-}
+// interface CounterProps {
+//   count: Signal<number>;
+// }
 
-export default function Counter(props: CounterProps) {
-  return (
-    <div class="flex gap-8 py-6">
-      <Button onClick={() => props.count.value -= 1}>-1</Button>
-      <p class="text-3xl tabular-nums">{props.count}</p>
-      <Button onClick={() => props.count.value += 1}>+1</Button>
-    </div>
-  );
-}
+// export default function Counter(props: CounterProps) {
+//   return (
+//     <div class="flex gap-8 py-6">
+//       <Button onClick={() => props.count.value -= 1}>-1</Button>
+//       <p class="text-3xl tabular-nums">{props.count}</p>
+//       <Button onClick={() => props.count.value += 1}>+1</Button>
+//     </div>
+//   );
+// }
