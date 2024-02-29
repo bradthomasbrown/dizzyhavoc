9c874533281789041d019822b7d77891992497c1
Brad Brown <bradbrown@bradbrown.llc>
2024-02-28 23:50:05 -0500

    preview bridge ui test

---
+++ routes/bridge.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div>
+      <div class="flex flex-col items-center justify-center overflow-hidden">
+        <div class="mb-5 w-[1920px] h-[400px] overflow-hidden flex justify-center items-center">
+          <image class="h-[600px] lg:h-auto" src="/dizzyhavoc_eye_3.jpeg"/>
+        </div>
+        <h1 class="text-4xl font-bold font-mono">dizzyhavoc-bridge</h1>
+        
+      </div>
+    </div>
+  );
+}
