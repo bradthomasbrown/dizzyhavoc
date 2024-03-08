a871f1cbda91a2361aa79326ffcbbabeb1faadb2
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-03-08 08:05:47 +0100

    fixed socials button too deep in z index

--- routes/index.tsx
+++ routes/index.tsx
@@ -53,14 +53,14 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div class="absolute lg:top-4 top-[6.8rem] left-1 sm:left-4">
+        <div class="absolute lg:top-4 top-[6.8rem] z-20 left-1 sm:left-4">
         <a
             class="bottom-0"
             target="_blank"
             href="https://linktr.ee/dizzyhavoc"
           >
         <div
-              className="text-lg text-[#3d3d3d] dark:text-[#d2d2d2] dark:bg-[#191919] text-center w-[100px] shadow-md dark:shadow-[#bfbfbf04] shadow-[#e9e9e9] font-[Poppins] rounded-lg hover:scale-[105%] dark:border-[#d2d2d228] border border-[#e9e9e9] cursor-pointer bg-[#f1f1f1]"
+              className="text-lg text-[#3d3d3d] dark:text-[#d2d2d2] dark:bg-[#191919] text-center w-[100px] shadow-md dark:shadow-[#bfbfbf04] shadow-[#e9e9e9] font-[Poppins] rounded-lg hover:scale-[105%] dark:border-[#d2d2d228] border z-20 border-[#e9e9e9] cursor-pointer bg-[#f1f1f1]"
             >
             🌐Socials
             </div>
