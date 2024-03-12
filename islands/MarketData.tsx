1f904aa8e2a9b36cff105c3fe044bfb5c51f8cd4
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-03-12 16:41:45 +0100

    padding

--- islands/MarketData.tsx
+++ islands/MarketData.tsx
@@ -13,7 +13,7 @@ export default function MarketData() {
           </div>
         </div>
         <div class="flex h-[20rem] mt-[4%] max-w-[480px] mx-auto shadow-lg rounded-xl bg-blur2">
-          <div class="w-full p-4">
+          <div class="w-full p-[10px]">
             <TokenData />
           </div>
         </div>
