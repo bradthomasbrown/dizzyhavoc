fdef365e0d7768ce18c056f6b9759cdc89cc3974
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-03-15 23:04:29 +0100

    spacing

--- routes/about.tsx
+++ routes/about.tsx
@@ -6,6 +6,8 @@ export default function About() {
       <p
         class="
   unselectable
+  mt-[3rem]
+  sm:mt-0
   text-[1.4rem]
   text-center
   dark:text-[#d0d0d0]
@@ -15,12 +17,13 @@ export default function About() {
   bg-transparent
   rounded-xl
   px-6
-  mb-[1.5rem]"
+  mb-0
+  sm:mb-[1.5rem]"
       >
         An open-source project driven by,
       </p>
       <div class="flex mt-[2rem] flex-row">
-        <img class="hover:scale-[101%] size-[120px] rounded-full" src="/misc/spider.png"/>
+        <img class="hover:scale-[101%] size-[90px] sm:size-[120px] rounded-full" src="/misc/spider.png"/>
         <div class="flex mx-5 flex-col">
         <h1 class="text-[1.5rem] font-[Poppins] font-medium z-10 font-Poppins dark:text-[#d2d2d2] text-[#282828]">Brad Brown</h1>
         <h1 class="text-[1.1rem] font-[Poppins] z-10 font-Poppins dark:text-[#d2d2d2] text-[#282828]">Creator, Lead developper, Back End</h1>
@@ -32,7 +35,7 @@ export default function About() {
         </div>
         </div>
         <div class="flex mt-[2rem] flex-row">
-        <img class="hover:scale-[101%] size-[100px] rounded-full" src="/misc/corv.jpg"/>
+        <img class="hover:scale-[101%] size-[70px] sm:size-[100px] rounded-full" src="/misc/corv.jpg"/>
         <div class="flex mx-5 flex-col">
         <h1 class="text-[1.5rem] font-[Poppins] font-medium z-10 font-Poppins dark:text-[#d2d2d2] text-[#282828]">Corvardt</h1>
         <h1 class="text-[1.1rem] font-[Poppins] z-10 font-Poppins dark:text-[#d2d2d2] text-[#282828]">Front End</h1>
@@ -59,6 +62,23 @@ export default function About() {
   mt-[1.5rem]"
       >
         And all the Discord, Github & Telegram contributors.
+      </p>
+      <p
+        class="
+  unselectable
+  text-[0.9rem]
+  text-center
+  dark:text-[#d0d0d0]
+  text-[#3d3d3d]
+  mx-auto
+  font-[monospace]
+  py-3
+  bg-transparent
+  rounded-xl
+  px-6
+  mt-[0.1rem]"
+      >
+        Built with machine code, Deno Fresh/KV & Tailwind.
       </p>
     </div>
     </>
