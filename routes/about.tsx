e9afdf693181aa131c965a3ea899ded556c51077
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-03-15 22:50:49 +0100

    updated links

--- routes/about.tsx
+++ routes/about.tsx
@@ -25,9 +25,9 @@ export default function About() {
         <h1 class="text-[1.5rem] font-[Poppins] font-medium z-10 font-Poppins dark:text-[#d2d2d2] text-[#282828]">Brad Brown</h1>
         <h1 class="text-[1.1rem] font-[Poppins] z-10 font-Poppins dark:text-[#d2d2d2] text-[#282828]">Creator, Lead developper, Back End</h1>
         <div class="flex flex-row">
-        <a target="_blank" href="https://github.com/corvardt" class="text-[1.2rem] ml-2 font-Poppins text-[#3b2d82] dark:text-[#ccb286]">github</a>
-        <a target="_blank" href="https://twitter.com/covardt" class="text-[1.2rem] ml-2 font-Poppins text-[#3b2d82] dark:text-[#ccb286]">twitter</a>
-        <a target="_blank" href="https://discordapp.com/users/209761949066461194" class="text-[1.2rem] ml-2 font-Poppins text-[#3b2d82] dark:text-[#ccb286]">discord</a>
+        <a target="_blank" href="https://github.com/bradbrown-llc" class="text-[1.2rem] ml-2 font-Poppins text-[#3b2d82] dark:text-[#ccb286]">github</a>
+        <a target="_blank" href="https://twitter.com/dizzyhavoc" class="text-[1.2rem] ml-2 font-Poppins text-[#3b2d82] dark:text-[#ccb286]">twitter</a>
+        <a target="_blank" href="https://discordapp.com/users/1173062401621504063" class="text-[1.2rem] ml-2 font-Poppins text-[#3b2d82] dark:text-[#ccb286]">discord</a>
         </div>
         </div>
         </div>
