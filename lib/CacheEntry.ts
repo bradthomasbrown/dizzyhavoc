f275917c0619525c9572f28b301c739def73ba26
Brad Brown <bradbrown@bradbrown.llc>
2024-03-22 17:15:00 -0400

    deprecate

--- lib/CacheEntry.ts
+++ lib/CacheEntry.ts
@@ -1,7 +1,7 @@
-export type CacheEntry<T> = {
-    value:T
-    timestamp:number
-    expireIn:number
-    kvKey:Deno.KvKey
-    gate:ReturnType<typeof Promise.withResolvers<void>>|null
-}
+// export type CacheEntry<T> = {
+//     value:T
+//     timestamp:number
+//     expireIn:number
+//     kvKey:Deno.KvKey
+//     gate:ReturnType<typeof Promise.withResolvers<void>>|null
+// }
