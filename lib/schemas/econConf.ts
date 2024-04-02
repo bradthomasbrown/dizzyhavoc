d43a399558aec481c9159a944fd3c75fd171890c
Brad Brown <bradbrown@bradbrown.llc>
2024-04-01 23:11:31 -0400

    accept bigint-able strings, transforming, or plain bigint

--- lib/schemas/econConf.ts
+++ lib/schemas/econConf.ts
@@ -1,7 +1,13 @@
 import z from "https://deno.land/x/zod@v3.22.4/index.ts";
 
 export const econConf = z.object({
-  gasLimitMultiplier: z.tuple([z.bigint(), z.bigint()]),
-  gasPriceMultiplier: z.tuple([z.bigint(), z.bigint()]),
-  baseFee: z.bigint(),
+  gasLimitMultiplier: z.tuple([
+    z.string().transform(BigInt).or(z.bigint()),
+    z.string().transform(BigInt).or(z.bigint())
+  ]),
+  gasPriceMultiplier: z.tuple([
+    z.string().transform(BigInt).or(z.bigint()),
+    z.string().transform(BigInt).or(z.bigint())
+  ]),
+  baseFee: z.string().transform(BigInt).or(z.bigint()),
 });
