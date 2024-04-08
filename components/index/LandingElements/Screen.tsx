e9d7703e65bdbf4345e8d7e2a07b0bfe9e1a7036
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-04-09 00:43:18 +0200

    trapeze gradient

--- components/index/LandingElements/Screen.tsx
+++ components/index/LandingElements/Screen.tsx
@@ -2,7 +2,8 @@ import Stars from "./Stars.tsx";
 export default function Screen() {
   return (
     <div class="h-[65%] sm:mt-5 mt-0 w-[99%]">
-      <div class="h-[97.5%] sm:mt-2 mt-0 sm:w-[50%] w-full overflow-hidden sm:p-5 p-0 justify-center mx-auto rounded-lg shadow-inner shadow-[#d6d6d6] dark:shadow-[#141414] bg-blur">
+      <div class="h-[97.5%] sm:mt-2 mt-0 sm:w-[75%] w-full overflow-hidden sm:p-5 p-0 justify-center
+       mx-auto rounded-lg shadow-inner shadow-[#d6d6d6] dark:shadow-[#141414] bg-blur">
         <Stars />
       </div>
     </div>
