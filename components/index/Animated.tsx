6b314f7d8ce75956b74f06c4110c9999df69fa2d
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-04-08 21:03:52 +0200

    small changes

--- components/index/Animated.tsx
+++ components/index/Animated.tsx
@@ -6,7 +6,7 @@ export default function Animated() {
       </div>
       <div class="flex flex-col h-screen justify-start sm:justify-center items-center">
         <div class="w-full flex justify-center">
-          <div class="flex flex-col mt-[13rem] items-center">
+          <div class="flex flex-col mt-[25rem] items-center">
             <div class="absolute min-w-full">
               <div class="w-full z-[10] shadow-xl">
                 <p class="font-medium font-[Poppins] unselectable z-10 w-full tracking-tighter italic absolute text-[1.5rem] sm:text-[2.3rem] text-center dark:text-[#d2d2d2] text-[#3d3d3d] px-2">
@@ -24,7 +24,7 @@ export default function Animated() {
               class="font-medium font-[Poppins] z-10 tracking-tighter sm:mt-[7.5rem] mt-[7.5rem] text-[1.1rem] sm:text-[1.7rem] text-center dark:text-[#d2d2d2] text-[#3d3d3d]"
               href="#info"
             >
-              <div class="cursor-pointer text-[#3b2d82] dark:text-[#ccb286] hover:scale-[105%]">
+              <div class="cursor-pointer unselectable text-[#3b2d82] dark:text-[#ccb286] hover:scale-[105%]">
                 Learn more
               </div>
             </a>
--- islands/common/navbar.tsx
+++ islands/common/navbar.tsx
@@ -3,7 +3,7 @@ import { IS_BROWSER } from "$fresh/runtime.ts";
 export default function NavBar() {
     if(!IS_BROWSER) return <></>;
   return (
-    <div class="sm:min-h-[4rem] min-h-[4.5rem] w-full sm:bg-transparent dark:sm:bg-transparent dark:bg-[#2828283f] bg-[#dbdbdb3f] sm:w-[45rem] justify-center mx-auto items-center px-4 flex  border-e-transparent border-s-transparent border-t-transparent dark:border-e-transparent dark:border-s-transparent dark:border-t-transparent dark:border-[#5e5e5e4d] border-[#dbdbdb] border">
+    <div class="sm:min-h-[4rem] min-h-[4.5rem] w-full sm:bg-transparent dark:sm:bg-transparent dark:bg-[#2828283f] bg-[#dbdbdb3f] sm:w-[45rem] justify-center mx-auto items-center px-4 flex border-e-transparent border-s-transparent border-t-transparent dark:border-e-transparent dark:border-s-transparent dark:border-t-transparent dark:border-[#5e5e5e4d] border-[#dbdbdb] border">
       <div class="justify-start w-full flex">
         <div class="lg:text-[1.8rem] unselectable sm:gap-2 gap-1 items-center flex justify-center text-[1.5rem] font-[Poppins] font-medium z-10 font-Poppins dark:text-[#d2d2d2] text-[#282828]">
           <a href="/">
