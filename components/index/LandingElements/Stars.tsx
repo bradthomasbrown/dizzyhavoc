72175e9c3f767f82e7a0071310de32b46b5f9140
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-04-09 00:33:56 +0200

    stars opacity + screen bg

--- components/index/LandingElements/Stars.tsx
+++ components/index/LandingElements/Stars.tsx
@@ -1,6 +1,6 @@
 export default function Stars(){
     return(
-        <div class="opacity-[30%] sm:opacity-100" id="stars-container">
+        <div class="opacity-[30%]" id="stars-container">
         <div id="stars"></div>
         <div id="stars2"></div>
         <div id="stars3"></div>
