77c8fee37bfd49e93db744f2998e887324c62cfe
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-04-09 19:13:41 +0200

    added Vertigo item

--- components/index/LandingElements/Screen.tsx
+++ components/index/LandingElements/Screen.tsx
@@ -1,10 +1,13 @@
 import Stars from "./Stars.tsx";
+import Vertigo from "./Vertigo.tsx";
 export default function Screen() {
   return (
-    <div class="h-[65%] sm:mt-5 mt-0 w-[99%]">
-      <div class="h-[90%] sm:mt-2 mt-0 sm:w-[75%] w-full overflow-hidden sm:p-5 p-0 justify-center
-       mx-auto rounded-lg shadow-inner shadow-[#d6d6d6] dark:shadow-[#141414] bg-blur4">
+    <div class="h-[65%] w-[99%] sm:mt-5 mt-0">
+      <div class="h-[90%] relative sm:w-[75%] w-full sm:mt-2 mt-0 sm:p-5 p-0 justify-center mx-auto overflow-hidden
+       rounded-lg shadow-inner shadow-[#d6d6d6] dark:shadow-[#141414] bg-blur4">
+        <Vertigo />
         <Stars />
+      
       </div>
     </div>
   );
