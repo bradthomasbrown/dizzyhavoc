fcc577b1d62795180110ed8a7e6385315ae51918
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-04-09 21:45:11 +0200

    mobile light pollution

--- components/index/LandingElements/Animations/Stars.tsx
+++ components/index/LandingElements/Animations/Stars.tsx
@@ -1,6 +1,6 @@
 export default function Stars(){
     return(
-        <div class="opacity-[60%]" id="stars-container">
+        <div class="sm:opacity-[60%] opacity-[15%]" id="stars-container">
         <div id="stars"></div>
         <div id="stars2"></div>
         <div id="stars3"></div>
