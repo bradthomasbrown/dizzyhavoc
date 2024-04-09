425adb0a2bd76d63008946be6e812efcf5373d1c
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-04-09 20:06:56 +0200

    no drag

--- components/index/LandingElements/Vertigo.tsx
+++ components/index/LandingElements/Vertigo.tsx
@@ -2,7 +2,7 @@ export default function Vertigo(){
     return(
         <div class="z-30 w-full h-full absolute items-center flex flex-row">
         <div class="flex flex-col ml-[15%]">
-        <img class="size-[130px] contrast-75" src="/misc/dzhv-art-chevron-m.jpg" alt="dzhv chevron" />
+        <img class="size-[130px] contrast-75" draggable={false} src="/misc/dzhv-art-chevron-m.jpg" alt="dzhv chevron" />
         <p class="text-3xl dark:text-[#d2d2d2] text-[#3d3d3d] font-medium unselectable font-[Poppins]">Vertigo, a cutting-edge bridge for $DZHV.</p>
         <p class="text-2xl dark:text-[#d2d2d2] text-[#3d3d3d] italic unselectable font-[Poppins]">Fast and secure, with reduced fees and deep optimization.</p>
         <p class="text-xl dark:text-[#d2d2d2] text-[#3d3d3d] bold unselectable font-[Poppins]">Live on testnet, try it now <a class="text-[#3b2d82] dark:text-[#ccb286] bold" target="_blank" href="https://beta.dzhv.io/bridge">here.</a></p>
