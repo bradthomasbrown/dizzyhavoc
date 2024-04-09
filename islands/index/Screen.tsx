2ece9e537ba3677cbdc2608b4e48cfdd5a46b4a6
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-04-09 23:09:14 +0200

    added countdown

--- islands/index/Screen.tsx
+++ islands/index/Screen.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'preact/hooks';
+import { IS_BROWSER } from "$fresh/runtime.ts";
 import Stars from "../../components/index/LandingElements/Animations/Stars.tsx";
 import Vertigo from "../../components/index/LandingElements/ScreenItems/Vertigo.tsx";
 import Mayhem from "../../components/index/LandingElements/ScreenItems/Mayhem.tsx";
@@ -6,14 +7,29 @@ import Mayhem from "../../components/index/LandingElements/ScreenItems/Mayhem.ts
 const items = [Vertigo, Mayhem];
 
 export default function Screen() {
+  
+  if (!IS_BROWSER) return null;
   const [currentItemIndex, setCurrentItemIndex] = useState(0);
-
+  const [timer, setTimer] = useState(9);
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentItemIndex((prevIndex) => (prevIndex + 1) % items.length);
-    }, 8000);
-
-    return () => clearInterval(interval);
+    }, 10000); // cycle every 10 seconds
+  
+    const interval2 = setInterval(() => {
+      setTimer((prevTimer) => {
+        if (prevTimer == 0) {
+          return 9; // Reset to 10 when timer reaches 0
+        } else {
+          return prevTimer - 1; // Countdown from current value
+        }
+      });
+    }, 1000);
+  
+    return () => {
+      clearInterval(interval);
+      clearInterval(interval2); // Clear the countdown interval
+    };
   }, []);
 
   const CurrentItem = items[currentItemIndex];
@@ -23,6 +39,8 @@ export default function Screen() {
       <div className="h-[90%] relative sm:w-[75%] w-full sm:mt-2 mt-0 sm:p-5 p-0 justify-center mx-auto overflow-hidden rounded-lg shadow-inner shadow-[#d6d6d6] dark:shadow-[#141414] bg-blur4">
         <CurrentItem />
         <Stars />
+        <div class="text-xl text-[#3d3d3d] dark:text-[#d4d4d4] font-[monospace] bottom-0 absolute">{timer}</div>
+ 
       </div>
     </div>
   );
