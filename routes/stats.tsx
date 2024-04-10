bdd375df22d1c7bbef9577b993b17dd7cc9add91
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-04-10 18:53:42 +0200

    less margin for stats

--- routes/stats.tsx
+++ routes/stats.tsx
@@ -1,7 +1,7 @@
 import ActiveContainer from "../islands/stats/ActiveContainer.tsx";
 export default function Stats() {
   return (
-      <div class="h-full sm:mb-[230px] mb-[100px] sm:h-screen translate-y-0 sm:translate-y-[7%]">
+      <div class="h-full sm:h-screen translate-y-0 sm:translate-y-[7%]">
               <p
         class="mb-[0.5rem]
   font-medium
