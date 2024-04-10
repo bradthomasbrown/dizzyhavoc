7ecc9c7a398f7931bd49cbfa74d0cf0f65d67f57
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-04-10 17:02:26 +0200

    added pause

--- islands/index/Screen.tsx
+++ islands/index/Screen.tsx
@@ -10,28 +10,38 @@ export default function Screen() {
   if (!IS_BROWSER) return null;
   const [currentItemIndex, setCurrentItemIndex] = useState(0);
   const [timer, setTimer] = useState(0);
+  const [paused, setIsTimerPaused] = useState(false);
+  const handleScreenClick = () => {
+    setIsTimerPaused((prevIsPaused) => !prevIsPaused);
+  };
+
   useEffect(() => {
     const interval = setInterval(() => {
-      setTimer((prevTimer) => {
-        if (prevTimer >= 100) {
-          setCurrentItemIndex((prevIndex) => (prevIndex + 1) % items.length);
-          return 0;
-        } else {
-          console.log(prevTimer + 0.1);
-          return prevTimer + 0.1;
-        }
-      });
+      if (!paused) {
+        setTimer((prevTimer) => {
+          if (prevTimer >= 100) {
+            setCurrentItemIndex((prevIndex) => (prevIndex + 1) % items.length);
+            return 0;
+          } else {
+            console.log(prevTimer + 0.1);
+            return prevTimer + 0.1;
+          }
+        });
+      }
     }, 10);
     return () => {
       clearInterval(interval);
     };
-  }, []);
+  }, [paused]);
 
   const CurrentItem = items[currentItemIndex];
 
   return (
     <div className="h-[65%] w-[99%] sm:mt-5 mt-0">
-      <div className="sm:h-[90%] h-[80svh] relative sm:w-[75%] w-full sm:mt-2 mt-0 sm:p-5 p-0 justify-center mx-auto overflow-hidden rounded-lg shadow-inner shadow-[#d6d6d6] dark:shadow-[#141414] bg-blur4">
+      <div
+        onClick={handleScreenClick}
+        className="sm:h-[90%] h-[80svh] relative sm:w-[75%] w-full sm:mt-2 mt-0 sm:p-5 p-0 justify-center mx-auto overflow-hidden rounded-lg shadow-inner shadow-[#d6d6d6] dark:shadow-[#141414] bg-blur4"
+      >
         <div class="bottom-[2px] px-[4px] absolute left-0 bg-blur3 items-center justify-center rounded-xl h-[1px] w-full">
           <div
             class="h-[1px] bg-[#3d3d3d] dark:bg-[#d0d0d0] rounded-xl"
