d293ac92be854b4dadd75cf5e616f3ad0500c947
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-04-11 22:04:16 +0200

    fixes for macbook air + deno fmt

--- components/index/LandingElements/Tagline.tsx
+++ components/index/LandingElements/Tagline.tsx
@@ -1,7 +1,7 @@
 export default function Tagline() {
   return (
-    <div class="text-center sm:mt-0 mt-9">
-          <p class="font-medium font-[Poppins] unselectable w-full tracking-tighter text-[1.4rem] sm:text-[2.6rem] dark:text-[#d2d2d2] text-[#3d3d3d]">
+    <div class="text-center sm:mt-3 mt-9">
+          <p class="font-medium font-[Poppins] unselectable w-full tracking-tighter text-[1.4rem] sm:text-[2.3rem] dark:text-[#d2d2d2] text-[#3d3d3d]">
             Upgradeable, machine-coded*, multi-chain, DeFi/GameFi ecosystem.
           </p>
         </div>  
--- islands/index/scrollTop.tsx
+++ islands/index/scrollTop.tsx
@@ -1,13 +1,19 @@
-export default function ScrollTop(){
-    function Scroll(){
-        const navbarElement = globalThis.document ? globalThis.document.getElementById('navbar') : null;
-        navbarElement && navbarElement.scrollIntoView({ behavior: 'smooth' });
-      }
-    return(
-        <div onClick={() => Scroll()}>
-            <div class="cursor-pointer active:scale-[98%] pt-[5rem]">
-            <img src="/misc/arrowup.svg" alt="" class="h-[40px] w-[40px] contrast-[0.3] invert-0 dark:invert" />
-        </div>
-        </div>
-    )
+export default function ScrollTop() {
+  function Scroll() {
+    const navbarElement = globalThis.document
+      ? globalThis.document.getElementById("navbar")
+      : null;
+    navbarElement && navbarElement.scrollIntoView({ behavior: "smooth" });
+  }
+  return (
+    <div onClick={() => Scroll()}>
+      <div class="cursor-pointer active:scale-[98%] pt-[5rem]">
+        <img
+          src="/misc/arrowup.svg"
+          alt=""
+          class="h-[40px] w-[40px] contrast-[0.3] invert-0 dark:invert"
+        />
+      </div>
+    </div>
+  );
 }
