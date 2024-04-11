527f30383341c63f874592bb4df7dd08c5308b7f
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-04-11 21:59:18 +0200

    added skip button

--- islands/index/Screen.tsx
+++ islands/index/Screen.tsx
@@ -4,13 +4,14 @@ import Stars from "../../components/index/LandingElements/Animations/Stars.tsx";
 import Vertigo from "../../components/index/LandingElements/ScreenItems/Vertigo.tsx";
 import Mayhem from "../../components/index/LandingElements/ScreenItems/Mayhem.tsx";
 
-const items = [Vertigo, Mayhem];
+const items = [Vertigo, Mayhem]; // Add more screen items here
 
 export default function Screen() {
   if (!IS_BROWSER) return null;
   const [currentItemIndex, setCurrentItemIndex] = useState(0);
   const [timer, setTimer] = useState(0);
   const [paused, setIsTimerPaused] = useState(false);
+
   const handleScreenClick = () => {
     setIsTimerPaused((prevIsPaused) => !prevIsPaused);
   };
@@ -33,6 +34,11 @@ export default function Screen() {
     };
   }, [paused]);
 
+  function Skip() {
+    setCurrentItemIndex((prevIndex) => (prevIndex + 1) % items.length);
+    setTimer(0);
+  }
+
   const CurrentItem = items[currentItemIndex];
   const loadingbar = (
     <div class="bottom-1 px-[4px] absolute left-0 bg-blur3 items-center justify-center rounded-xl h-[1px] w-full">
@@ -46,15 +52,23 @@ export default function Screen() {
   return (
     <div className="h-full sm:h-[30rem] w-full sm:mt-5 mt-0">
       <div
-        onClick={handleScreenClick}
+        onClick={() => handleScreenClick()}
         className="sm:min-h-[30rem] sm:max-h-[45rem] min-h-[60svh] max-h-[60svh] relative xl:w-[70%] w-full sm:mt-2 mt-0 sm:p-5 p-0 justify-center mx-auto overflow-hidden rounded-lg shadow-inner shadow-[#d6d6d6] dark:shadow-[#141414] bg-blur4"
       >
         {loadingbar}
         <p class="font-medium font-[Poppins] italic absolute top-0 left-0 m-5 unselectable w-full tracking-tighter text-[0.8rem] sm:text-[1.7rem] text-start dark:text-[#969696cc] text-[#636363cc]">
-            DeFi user or developer? We provide useful tools for everyone.
-          </p>
+          DeFi user or developer? We provide useful tools for everyone.
+        </p>
         <CurrentItem />
         <Stars />
+        <div
+          id="skip"
+          class="font-medium z-50 font-[Poppins] m-5 active:scale-[98%] absolute cursor-pointer unselectable bottom-0 left-0 tracking-tighter text-[0.8rem] sm:text-[1.7rem] text-start dark:text-[#969696cc] text-[#636363cc]"
+          onClick={Skip}
+          onClickCapture={handleScreenClick} // 🤔
+        >
+          <img src="/misc/arrowright.svg" class="dark:invert invert-0 contrast-50 scale-[1.7]"/>
+        </div>
       </div>
     </div>
   );
