16a634243aec2a1597e7b7c5a11179cdd75e6259
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-04-13 01:36:44 +0200

    added background for landing

---
+++ components/index/Background.tsx
@@ -0,0 +1,19 @@
+export default function Background() {
+  return (
+    <div class="bg-[url('/misc/landing.png')] 
+        opacity-20 
+        dark:opacity-5 
+        dark:saturate-0 
+        saturate-100 
+        invert 
+        dark:invert-0 
+        bg-cover 
+        bg-center 
+        bg-no-repeat 
+        w-full 
+        h-full 
+        absolute 
+        top-0 
+        left-0" />
+  );
+}
--- components/index/Landing.tsx
+++ components/index/Landing.tsx
@@ -1,10 +1,12 @@
 import Screen from "../../islands/index/Screen.tsx";
 import Tagline from "../index/LandingElements/Tagline.tsx";
+import Background from "../index/Background.tsx";
 export default function Landing() {
   return (
     <div>
-      <div class="flex flex-col h-screen justify-center items-center">
-      <Tagline />
+      <div class="flex relative flex-col h-screen justify-center items-center">
+        <Background />
+        <Tagline />
         <Screen />
       </div>
     </div>
