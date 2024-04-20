801a943a012cc6eaede6428eaf9fdedcf9cc9783
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-04-20 19:41:48 +0200

    removed factory for now

--- components/index/Landing/ScreenItems/Factory.tsx
+++ components/index/Landing/ScreenItems/Factory.tsx
@@ -5,9 +5,8 @@ export function Factory(){
         <img class="sm:size-[100px] ml-[1rem] sm:ml-0 mb-2 size-[90px] contrast-75" draggable={false} src="/misc/Factory.png" alt="GameFI" />
         </div>
         <div class="flex flex-col ml-[1rem]">
-        <p class="sm:text-4xl text-2xl dark:text-[#d2d2d2] text-[#3d3d3d] font-medium unselectable font-[Poppins]">ERC314 token Factory</p>
+        <p class="sm:text-4xl text-2xl dark:text-[#d2d2d2] text-[#3d3d3d] font-medium unselectable font-[Poppins]">ERC token Factory</p>
         <p class="sm:text-xl text-md dark:text-[#d2d2d2] text-[#3d3d3d] bold unselectable font-[Poppins]">With drastically reduced fees/costs.</p>
-        <p class="sm:text-lg text-sm dark:text-[#d2d2d2] text-[#3d3d3d] italic unselectable font-[Poppins]">A collaboration with EV Terminal.</p>
         </div>
       </div>
     )
