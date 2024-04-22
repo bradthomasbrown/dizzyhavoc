a48ec2163ac7858e1e919951c775d5f02217edf5
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-04-22 21:59:31 +0200

    fixed fallback key

--- lib/stats/Holders.tsx
+++ lib/stats/Holders.tsx
@@ -1,5 +1,5 @@
 export async function GetHolders(url: string) {
-    const options = {method: 'GET', headers: {accept: 'application/json', 'x-api-key': Deno ? Deno.env.get("chainbase_api") : "demo"}};
+    const options = {method: 'GET', headers: {accept: 'application/json', 'x-api-key': typeof Deno !== 'undefined' ? Deno.env.get("chainbase_api") : "demo"}};
     try {
       const response = await fetch(url, options)
       const data = await response.json();
