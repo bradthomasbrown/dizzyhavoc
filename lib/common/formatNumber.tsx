be5066419638646ddd1c5a3e852a108a8fd8b4f8
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-04-22 23:12:54 +0200

    slice ".00"

--- lib/common/formatNumber.tsx
+++ lib/common/formatNumber.tsx
@@ -1,16 +1,18 @@
-export function formatNumber(num: number, precision = 2) { // format num in K, M, B
-    const map = [
-      { suffix: "T", threshold: 1e12 },
-      { suffix: "B", threshold: 1e9 },
-      { suffix: "M", threshold: 1e6 },
-      { suffix: "K", threshold: 1e3 },
-      { suffix: "", threshold: 1 },
-    ];
-    const found = map.find((x) => Math.abs(num) >= x.threshold);
-    if (found) {
-      const formatted =
-        (num / found.threshold).toFixed(precision) + found.suffix;
-      return formatted;
+export function formatNumber(num: number, precision = 2) {
+  const map = [
+    { suffix: "T", threshold: 1e12 },
+    { suffix: "B", threshold: 1e9 },
+    { suffix: "M", threshold: 1e6 },
+    { suffix: "K", threshold: 1e3 },
+    { suffix: "", threshold: 1 },
+  ];
+  const found = map.find((x) => Math.abs(num) >= x.threshold);
+  if (found) {
+    let formatted = (num / found.threshold).toFixed(precision) + found.suffix;
+    if (formatted.endsWith(".00")) {
+      formatted = formatted.slice(0, -3);
     }
-    return num;
+    return formatted;
   }
+  return num;
+}
