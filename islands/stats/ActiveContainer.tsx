eb6c4a8966c33cbd8d65ba64754280ebe8140e46
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-04-24 23:27:19 +0200

    Summary always on

--- islands/stats/ActiveContainer.tsx
+++ islands/stats/ActiveContainer.tsx
@@ -6,13 +6,18 @@ import { Average } from "./charts/weeklyAverage.tsx";
 import MarketBarsContainer from "./MarketBarsContainer.tsx";
 export default function ActiveContainer() {
   const [currentItemIndex, setCurrentItemIndex] = useState(0);
-  const items = [Summary, Average, MarketCap, Liquidity];
+  const items = [Average, MarketCap, Liquidity];
   function Skip() {
     setCurrentItemIndex((prevIndex) => (prevIndex + 1) % items.length);
   }
   const CurrentItem = items[currentItemIndex];
   return (
-    <div class="flex w-full mt-[1rem] mx-auto sm:w-[42rem] vignets flex-col">
+    <div class="flex w-full mt-[1rem] mx-auto sm:w-[42rem] gap-y-2 vignets flex-col">
+      <div class="flex h-[4.5rem] sm:h-[5rem] mx-auto justify-center sm:w-[473px] w-[358px] bg-blur2 rounded-xl">
+        <div class="w-full p-1">
+          <Summary />
+        </div>
+      </div>
       <div class="flex h-[4.5rem] sm:h-[5rem] mx-auto justify-center sm:w-[473px] w-[358px] bg-blur2 rounded-xl">
         <div onclick={() => Skip()} class="absolute cursor-pointer active:scale-[98%] z-[2] sm:right-[3px] right-1 top-1 sm:top-8">
           <img
@@ -21,10 +26,10 @@ export default function ActiveContainer() {
           ></img>
         </div>
         <div class="w-full p-1">
-          
         <CurrentItem/>
         </div>
       </div>
+
       <div class="flex mt-[0.3rem] h-full w-full sm:mx-0 mx-auto shadow-none sm:shadow-[0_0_15px_0_rgba(0,0,0,0.07)] rounded-xl bg-blur2">
         <div class="w-full p-1">
           <MarketBarsContainer />
