829b1a3700dd795c92a2b7249ff6952d72c5ed19
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-04-24 14:09:50 +0200

    unselectable

--- islands/stats/charts/weeklyLiquidity.tsx
+++ islands/stats/charts/weeklyLiquidity.tsx
@@ -46,7 +46,7 @@ export function Liquidity() {
 
     return (
       <>
-       <p class="font-[Poppins] text-[10px] text-center pl-1 absolute">Weekly Liquidity</p>
+       <p class="font-[Poppins] text-[10px] unselectable text-center pl-1 absolute">Weekly Liquidity</p>
         <div class="unselectable vignets absolute bottom-0 sm:h-[65px] sm:w-[455px] h-[60px] w-[345px]">
           {fetchedData.value && fetchedData.value.length > 0 && (
             <Chart
