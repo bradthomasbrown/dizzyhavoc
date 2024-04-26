60ca33889387c6de66d1fad2d07dd20200f74327
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-04-26 22:38:37 +0200

    no lowercase

--- lib/stats/Requests/Weekly.tsx
+++ lib/stats/Requests/Weekly.tsx
@@ -1,7 +1,7 @@
 export async function Weekly(category: string){
     try {
         const response = await fetch(
-          "https://swift-panther.deno.dev/v1/weekly/"+category.toLowerCase(),
+          "https://swift-panther.deno.dev/v1/weekly/"+category,
         );
         const data = await response.json();
         return data;
