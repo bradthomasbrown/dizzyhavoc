8046b7bbcb283c6c307b8f03f559eddbdccebe29
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-05-04 21:58:04 +0200

    noise opacity

--- components/common/backgrounds/Noise.tsx
+++ components/common/backgrounds/Noise.tsx
@@ -2,13 +2,13 @@ export function Noise() {
   return (
     <div class="size-full absolute z-[-1]">
       <div class="mask top-0 sm:h-[10%] h-[7%] absolute w-full bg-gradient-to-t from-transparent to-[#ededed] dark:to-[#101010]"></div>
-      <div class="mask bottom-0 sm:h-[10%] h-[7%] absolute w-full bg-gradient-to-b from-transparent to-[#ededed] dark:to-[#101010]"></div>
+      <div class="mask bottom-0 sm:h-[12%] h-[7%] absolute w-full bg-gradient-to-b from-transparent to-[#ededed] dark:to-[#101010]"></div>
       <div
         class="
     dark:bg-[url('/misc/noise.png')]
     bg-[url('/misc/whitenoise.png')]
-        opacity-35
-        dark:opacity-20
+        opacity-50
+        dark:opacity-25
         bg-repeat
         size-full
         absolute 
