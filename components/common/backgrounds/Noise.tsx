5d3bcd828ef0f3943aeb9ebb64421258a381b178
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-05-04 19:36:19 +0200

    noise mask

--- components/common/backgrounds/Noise.tsx
+++ components/common/backgrounds/Noise.tsx
@@ -1,16 +1,19 @@
 export function Noise() {
   return (
-    <div class="
+    <div class="size-full absolute">
+      <div class="mask top-0 sm:h-[7%] h-[1rem] absolute w-full bg-gradient-to-t from-transparent to-[#ededed] dark:to-[#101010]"></div>
+      <div class="mask bottom-0 sm:h-[10%] h-[5%] absolute w-full bg-gradient-to-b from-transparent to-[#ededed] dark:to-[#101010]"></div>
+      <div
+        class="
     dark:bg-[url('/misc/noise.png')]
     bg-[url('/misc/whitenoise.png')]
-    mt-[4.5rem] sm:mt-[4rem]
         opacity-35
         dark:opacity-20
         bg-repeat
-        w-full 
-        h-full
+        size-full
         absolute 
-        top-0 
-        z-[-1]" />
+        z-[-1]"
+      />
+    </div>
   );
 }
