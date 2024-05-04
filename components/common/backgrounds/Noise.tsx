102bd7853c8378bd34f34d50c8a55d240560518a
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-05-04 20:17:11 +0200

    404 + noise z index

--- components/common/backgrounds/Noise.tsx
+++ components/common/backgrounds/Noise.tsx
@@ -1,6 +1,6 @@
 export function Noise() {
   return (
-    <div class="size-full absolute">
+    <div class="size-full absolute z-[-1]">
       <div class="mask top-0 sm:h-[7%] h-[1rem] absolute w-full bg-gradient-to-t from-transparent to-[#ededed] dark:to-[#101010]"></div>
       <div class="mask bottom-0 sm:h-[10%] h-[5%] absolute w-full bg-gradient-to-b from-transparent to-[#ededed] dark:to-[#101010]"></div>
       <div
--- routes/_404.tsx
+++ routes/_404.tsx
@@ -2,14 +2,13 @@ import { Noise } from "../components/common/backgrounds/Noise.tsx";
 export default function Error404() {
   return (
     <>
-    <div class="h-[20%]">      <Noise /></div>
+        <div class="h-full top-0"><Noise /></div>
 
-      <div class="h-full flex flex-col justify-center items-center">
+      <div class="h-screen flex flex-col justify-center items-center">
         <img
-          class="invert-0 dark:invert mt-[10%]"
+          class="invert-0 dark:invert sm:w-[450px] w-[250px]"
           src="/misc/1.gif"
           alt="404 image"
-          width="450"
         />
         <h1 class="sm:text-4xl text-2xl dark:text-[#d2d2d2] text-[#282828] font-bold">
           404 - Page not found
@@ -27,12 +26,8 @@ export default function Error404() {
         <a href="/about" class="underline dark:text-[#d2d2d2] text-[#282828] mb-[10%]">
           about
         </a>
-        <p class="dark:text-[#d2d2d2] text-[#282828]">|</p>
         <a href="/explore" class="underline dark:text-[#d2d2d2] text-[#282828] mb-[10%]">
           explore
-        </a>
-        <a href="/roadmap" class="underline dark:text-[#d2d2d2] text-[#282828] mb-[10%]">
-          roadmap
         </a>
         </div>
       </div>
