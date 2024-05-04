5f4d1f6c38137df3e659449409afb2a8854e50e7
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-05-04 18:45:48 +0200

    invert lib color when dark

--- components/index/Landing/CarouselItems/Mayhem.tsx
+++ components/index/Landing/CarouselItems/Mayhem.tsx
@@ -6,7 +6,7 @@ export function Mayhem(){
     return(
       <div class="z-[2] w-full h-full slidefadeout unselectable absolute">
         <div class="flex flex-col">
-        <img class="sm:size-[120px] vignets ml-[1rem] size-[90px] contrast-75" draggable={false} src="/carousel/Lib.png" alt="Mayhem" />
+        <img class="sm:size-[120px] vignets ml-[1rem] size-[90px] invert-0 dark:invert contrast-75" draggable={false} src="/carousel/Lib.png" alt="Mayhem" />
         </div>
       </div>
     )
