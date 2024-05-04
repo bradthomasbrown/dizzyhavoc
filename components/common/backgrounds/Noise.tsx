af57fe270cb8ced1de81a13aabda0c0ba0d0c0ce
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-05-04 21:51:13 +0200

    mask size

--- components/common/backgrounds/Noise.tsx
+++ components/common/backgrounds/Noise.tsx
@@ -1,8 +1,8 @@
 export function Noise() {
   return (
     <div class="size-full absolute z-[-1]">
-      <div class="mask top-0 sm:h-[7%] h-[1rem] absolute w-full bg-gradient-to-t from-transparent to-[#ededed] dark:to-[#101010]"></div>
-      <div class="mask bottom-0 sm:h-[10%] h-[5%] absolute w-full bg-gradient-to-b from-transparent to-[#ededed] dark:to-[#101010]"></div>
+      <div class="mask top-0 sm:h-[10%] h-[7%] absolute w-full bg-gradient-to-t from-transparent to-[#ededed] dark:to-[#101010]"></div>
+      <div class="mask bottom-0 sm:h-[10%] h-[7%] absolute w-full bg-gradient-to-b from-transparent to-[#ededed] dark:to-[#101010]"></div>
       <div
         class="
     dark:bg-[url('/misc/noise.png')]
