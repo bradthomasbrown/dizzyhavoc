1ed9948c99d29a9fde19b201ecdb2baa41a58aff
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-05-08 17:25:22 +0200

    footer mt

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
 <footer
   draggable={false}
   id="footer"
-  class="xl-invis select-none sm:h-[200px] h-[100px] bg-[#ededed] dark:bg-[#101010] border dark:border-1 border-1 dark:border-t-[#ededed18] dark:border-b-transparent border-b-transparent dark:border-s-transparent border-s-transparent dark:border-e-transparent border-e-transparent border-t-[#19191918] w-full flex flex-row justify-center"
+  class="xl-invis mt-0 sm:mt-[3rem] select-none sm:h-[200px] h-[100px] bg-[#ededed] dark:bg-[#101010] border dark:border-1 border-1 dark:border-t-[#ededed18] dark:border-b-transparent border-b-transparent dark:border-s-transparent border-s-transparent dark:border-e-transparent border-e-transparent border-t-[#19191918] w-full flex flex-row justify-center"
 >
       <div class="flex flex-col gap-4 justify-center">
         <div class="flex items-center gap-x-8">
