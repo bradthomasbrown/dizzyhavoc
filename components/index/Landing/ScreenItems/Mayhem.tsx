923a86e2bc7c14f44ff4e268f1bf1a48ef4db913
Corvardt <124924375+corvardt@users.noreply.github.com>
2024-05-10 19:52:36 +0200

    link size

--- components/index/Landing/ScreenItems/Mayhem.tsx
+++ components/index/Landing/ScreenItems/Mayhem.tsx
@@ -7,7 +7,7 @@ export function Mayhem(){
         <div class="flex flex-col ml-[1rem]">
         <p class="sm:text-4xl text-xl dark:text-[#d2d2d2] text-[#3d3d3d] font-medium unselectable font-[Poppins]">A new rich Web3 library.</p>
         <p class="sm:text-2xl text-lg dark:text-[#d2d2d2] text-[#3d3d3d] italic unselectable font-[Poppins]">Ejra & Vortex. A complete toolset for developers.</p>
-        <a class="" draggable={false} target="_blank" href="https://github.com/bradbrown-llc?tab=repositories"><div class="w-[90px] h-[30px] hover:scale-[102%] active:scale-[98%] mt-2 rounded-md dark:text-[#ccb286] text-[#b37635] bg-[#ccb28611] font-[Poppins] text-lg text-center font-light tracking-tight border dark:border-[#ccb286] border-[#b37635]">github</div></a>
+        <a class="w-[90px] h-[30px]" draggable={false} target="_blank" href="https://github.com/bradbrown-llc?tab=repositories"><div class="w-[90px] h-[30px] hover:scale-[102%] active:scale-[98%] mt-2 rounded-md dark:text-[#ccb286] text-[#b37635] bg-[#ccb28611] font-[Poppins] text-lg text-center font-light tracking-tight border dark:border-[#ccb286] border-[#b37635]">github</div></a>
         </div>
       </div>
     )
--- components/index/Landing/ScreenItems/Vertigo.tsx
+++ components/index/Landing/ScreenItems/Vertigo.tsx
@@ -7,7 +7,7 @@ export function Vertigo(){
         <div class="flex flex-col ml-[1rem]">
         <p class="sm:text-3xl text-xl dark:text-[#d2d2d2] text-[#3d3d3d] font-medium unselectable font-[Poppins]">Vertigo, a cutting-edge bridge for $DZHV.</p>
         <p class="sm:text-2xl text-md dark:text-[#d2d2d2] text-[#3d3d3d] italic unselectable font-[Poppins]">Fast and secure, with reduced fees and in-depth optimizations.</p>
-        <a class="" draggable={false} target="_blank" href="https://beta.dzhv.io/bridge"><div class="w-[190px] h-[30px] hover:scale-[102%] active:scale-[98%] mt-2 rounded-md dark:text-[#ccb286] text-[#b37635] bg-[#ccb28611] font-[Poppins] text-lg text-center font-light tracking-tight border dark:border-[#ccb286] border-[#b37635]">Try now on testnet</div></a>
+        <a class="w-[190px] h-[30px]" draggable={false} target="_blank" href="https://beta.dzhv.io/bridge"><div class="w-[190px] h-[30px] hover:scale-[102%] active:scale-[98%] mt-2 rounded-md dark:text-[#ccb286] text-[#b37635] bg-[#ccb28611] font-[Poppins] text-lg text-center font-light tracking-tight border dark:border-[#ccb286] border-[#b37635]">Try now on testnet</div></a>
         </div>
       </div>
     )
