968a56bb5d6cc13f3538613fb047efeafad7f269
Brad Brown <bradbrown@bradbrown.llc>
2024-05-24 04:14:13 -0400

    await all steps

--- tests/chain-w-dzhv/setup.ts
+++ tests/chain-w-dzhv/setup.ts
@@ -40,8 +40,8 @@ const erc20 = await steps.erc20({ session, nonce: 2n, salt: 1n, create2 }) // de
 await wait(node, erc20.hash)
 const dzhv = await steps.dzhv({ session, nonce: 3n, salt: 2n, create2 }) // deploy dzhv
 
-const link = steps.link({ session, resolver, erc20, nonce: 0n }) // implement erc20
+const link = await steps.link({ session, resolver, erc20, nonce: 0n }) // implement erc20
 await wait(node, link.hash)
 
-const mint = steps.mint({ session, dzhv, nonce: 0n }) // wallet mint
+const mint = await steps.mint({ session, dzhv, nonce: 0n }) // wallet mint
 await wait(node, mint.hash)
