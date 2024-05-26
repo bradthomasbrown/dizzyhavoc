2867a77a7c35fc0d3526a1755286586649d0fffe
Brad Brown <bradbrown@bradbrown.llc>
2024-05-26 03:59:12 -0400

    update lib barrel, no more Signer and signRawTx here

--- lib/mod.ts
+++ lib/mod.ts
@@ -1,4 +1,2 @@
 export { default as getC2Addr } from './getC2Addr.ts'
 export { default as getCode } from './getCode.ts'
-export { default as Signer } from './Signer.ts'
-export { default as signRawTx } from './signRawTx.ts'
