568eeb4eedb92fd7d92cf15e997c73b828ffa5fb
Brad Brown <bradbrown@bradbrown.llc>
2024-08-18 22:15:41 +0000

    won't need

--- lib/opcodes.ts
+++
@@ -1,5 +0,0 @@
-export default {
-    STOP: { opcode: 0x00, input: [], output: [] },
-    ADD: { opcode: 0x01, input: ['a', 'b'], output: ['a + b'] },
-    PUSH1: { opcode: 0x60, input: [], output: ['value'] }
-} as const
